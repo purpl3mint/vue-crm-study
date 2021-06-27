24969cda4fa289e54ee42985d768f80009c5c870
Dmitry <purpl3mint@yandex.ru>
2021-06-28 01:07:43 +0700

    Added register validation and login messages

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import messagePlugin from "@/utils/message.plugin";
 import dateFilter from "@/filters/date.filter";
 import "materialize-css/dist/js/materialize.min";
 
 Vue.config.productionTip = false;
 
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
