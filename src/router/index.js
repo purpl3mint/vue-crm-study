0baa97d901d8fce798c12395e09f92a71fb01f3b
Dmitry <purpl3mint@yandex.ru>
2021-06-27 12:46:13 +0700

    Added layouts

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,8 +7,57 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    meta: {layout: 'main'},
+    component: () => import("../views/Home.vue")
   },
+  {
+    path: "/login",
+    name: "Login",
+    meta: {layout: 'empty'},
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/categories",
+    name: "Categories",
+    meta: {layout: 'main'},
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/detail-record",
+    name: "DetailRecord",
+    meta: {layout: 'main'},
+    component: () => import("../views/DetailRecord.vue")
+  },
+  {
+    path: "/history",
+    name: "History",
+    meta: {layout: 'main'},
+    component: () => import("../views/History.vue")
+  },
+  {
+    path: "/planning",
+    name: "Planning",
+    meta: {layout: 'main'},
+    component: () => import("../views/Planning.vue")
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    meta: {layout: 'main'},
+    component: () => import("../views/Profile.vue")
+  },
+  {
+    path: "/record",
+    name: "Record",
+    meta: {layout: 'main'},
+    component: () => import("../views/Record.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    meta: {layout: 'empty'},
+    component: () => import("../views/Register.vue")
+  }
 ];
 
 const router = new VueRouter({
