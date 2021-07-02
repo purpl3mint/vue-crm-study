426122f16bff070c9b016e710c93f778415cb28a
Dmitry <purpl3mint@yandex.ru>
2021-07-02 19:20:03 +0700

    Added categories creation

--- src/store/index.js
+++ src/store/index.js
@@ -1,33 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import auth from './auth';
-import info from './info';
+import auth from "./auth";
+import info from "./info";
+import category from "./category";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    error: null
+    error: null,
   },
   mutations: {
     setError(state, error) {
-      state.error = error
+      state.error = error;
     },
     clearError(state) {
-      state.error = null
-    }
+      state.error = null;
+    },
   },
   actions: {
     async fetchCurrency() {
-      const key = process.env.VUE_APP_FIXER
-      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
-      return await res.json()
-    }
+      const key = process.env.VUE_APP_FIXER;
+      const res = await fetch(
+        `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`
+      );
+      return await res.json();
+    },
   },
   getters: {
-    error: s => s.error
+    error: (s) => s.error,
   },
   modules: {
-    auth, info
+    auth,
+    info,
+    category,
   },
 });
