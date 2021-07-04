15d74fb2810eeee5d834c034ff646dd5eeb35dd6
Dmitry <purpl3mint@yandex.ru>
2021-07-04 16:48:24 +0700

    Added detail pages

--- src/store/record.js
+++ src/store/record.js
@@ -1,25 +1,25 @@
-import firebase from "firebase/app"
+import firebase from "firebase/app";
 
 export default {
   actions: {
-    async createRecord({dispatch, commit}, record) {
+    async createRecord({ dispatch, commit }, record) {
       try {
-        const uid = await dispatch('getUid')
-        return await firebase.database().ref(`/users/${uid}/records`).push(record)
+        const uid = await dispatch("getUid");
+        return await firebase
+          .database()
+          .ref(`/users/${uid}/records`)
+          .push(record);
       } catch (e) {
-        commit('setError', e)
-        throw e
+        commit("setError", e);
+        throw e;
       }
     },
-    async fetchRecords({dispatch, commit}) {
+    async fetchRecords({ dispatch, commit }) {
       try {
         const uid = await dispatch("getUid");
         const records =
           (
-            await firebase
-              .database()
-              .ref(`/users/${uid}/records`)
-              .once("value")
+            await firebase.database().ref(`/users/${uid}/records`).once("value")
           ).val() || {};
 
         return Object.keys(records).map((key) => ({
@@ -27,9 +27,23 @@ export default {
           id: key,
         }));
       } catch (e) {
-        commit('setError', e)
-        throw e
+        commit("setError", e);
+        throw e;
       }
-    }
-  }
-}
+    },
+    async fetchRecordById({ dispatch, commit }, id) {
+      try {
+        const uid = await dispatch("getUid");
+        const record =
+          (
+            await firebase.database().ref(`/users/${uid}/records`).child(id).once("value")
+          ).val() || {};
+
+        return {...record, id}
+      } catch (e) {
+        commit("setError", e);
+        throw e;
+      }
+    },
+  },
+};
