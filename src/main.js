c71d77f89b1d6573bc15962e774819c3742aba0c
Dmitry <purpl3mint@yandex.ru>
2021-07-05 17:28:48 +0700

    Added some localization

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ import Loader from "@/components/app/Loader"
 import messagePlugin from "@/utils/message.plugin";
 import dateFilter from "@/filters/date.filter";
 import currencyFilter from "@/filters/currency.filter";
+import localizeFilter from "@/filters/localize.filter";
 import tooltipDirective from "@/directives/tooltip.directive";
 import "materialize-css/dist/js/materialize.min";
-
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
@@ -22,6 +22,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
